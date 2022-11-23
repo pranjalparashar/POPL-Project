e68608293126f2d3895905fe85bd0abf73910395
pranjalparashar <75277556+pranjalparashar@users.noreply.github.com>
2022-11-23 17:16:37 +0530

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,10 +1,16 @@
+
 // add a library to the program
 // always done at the program top
 function includeLibrary() {
-  libName = ''; 
+
+  libName = ''; //name of the library to be added
+
+  //determine name by combining all the words after 'include'
   for (var i = 1; i < splitWords.length; i++) {
       libName += splitWords[i].toLowerCase();
   }
+  
+  // add the text to the program textarea
   programTextArea.executeEdits("", [{
     range: {
         startLineNumber: 1,
@@ -13,7 +19,9 @@ function includeLibrary() {
         endColumn: 1,
     },
     text: '#include <' + libName + '.h>\n',
+    //only move marker if this is the first library we are including. Otherwise, leave the cursor in the middle of the program
     forceMoveMarkers: programTextArea.getPosition().lineNumber === 1 ? true : false
   }]);
+
   includeStatements += '#include <' + libName + '.h>\n';
 }
